1f8a1e658e0aaa763b7132e3d66063e0514f5cf7
Бакыт <35407872+kurbanaliev@users.noreply.github.com>
2019-07-08 17:37:29 +0300

    fixed modal

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var button = $('#button');
   var modal = $('#modal');
   var close = $('#close');
-  
+
   button.on('click', function(){
     modal.addClass('modal_active');
   });
